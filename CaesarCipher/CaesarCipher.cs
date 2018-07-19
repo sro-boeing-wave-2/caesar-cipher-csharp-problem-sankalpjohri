8aa3fc0ede05359c1d82a27e5b318981da9b171a
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-19 14:44:24 +0530

    Fix for Issue #1

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace CaesarCipher
@@ -8,7 +9,7 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            String encryptedString = "";
+            StringBuilder encryptedStringBuilder = new StringBuilder();
             if (!String.IsNullOrWhiteSpace(text))
             {
                 foreach (char c in text)
@@ -16,15 +17,15 @@ namespace CaesarCipher
                     if (Regex.IsMatch(Char.ToString(c), @"[a-zA-Z]"))
                     {
                         char result = (char) (c + (shiftKey % 26));
-                        encryptedString = String.Concat(encryptedString, (char) (((char.IsUpper(c) && result > 90) || (char.IsLower(c) && result > 122)) ? result - 26 : result));
+                        encryptedStringBuilder.Append((char)(((char.IsUpper(c) && result > 90) || (char.IsLower(c) && result > 122)) ? result - 26 : result));
                     }
                     else
                     {
-                        encryptedString = String.Concat(encryptedString, c);
+                        encryptedStringBuilder.Append(c);
                     }
                 }
             }
-            return encryptedString;
+            return encryptedStringBuilder.ToString();
         }
     }
 }
